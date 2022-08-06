f18820f911b0cc3e6fe8121afb7a63b460817a0e
Fus1onDev <luke_s1220@yahoo.co.jp>
2022-08-06 23:45:49 +0900

    first commit

---
+++ EjectKey/EjectKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EjectKeyApp.swift
+//  EjectKey
+//
+//  Created by Fus1onDev on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct EjectKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
